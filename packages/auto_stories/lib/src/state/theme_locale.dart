2c2ac706aa424a8d8c1ab5b7984c0712eb4209a6
James Aprosail <aprosail@outlook.com>
2024-12-26 15:48:40 +0800

    feat: adaptive locale theme

--- packages/auto_stories/lib/src/state/theme_locale.dart
+++ packages/auto_stories/lib/src/state/theme_locale.dart
@@ -1,4 +1,71 @@
+import 'package:auto_stories/src/utils.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
+
+import 'binding.dart';
+
+extension WrapLocale on Widget {
+  // ignore: unnecessary_this readability.
+  Inherit<T> localeAs<T extends LocaleThemeBase>(T locale) => this
+      .textDirection(locale.direction) //
+      .inherit(locale);
+
+  AdaptiveLocale<T> adaptiveLocale<T extends LocaleThemeBase>(
+    LocaleAdapter<T> adapter, {
+    Key? key,
+  }) {
+    return AdaptiveLocale(
+      adapter: adapter,
+      child: this,
+    );
+  }
+}
+
+class AdaptiveLocale<T extends LocaleThemeBase> extends StatefulWidget {
+  const AdaptiveLocale({super.key, required this.adapter, required this.child});
+
+  final LocaleAdapter<T> adapter;
+  final Widget child;
+
+  @override
+  State<AdaptiveLocale<T>> createState() => _AdaptiveLocaleState();
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(DiagnosticsProperty<LocaleAdapter<T>>('adapter', adapter));
+  }
+}
+
+class _AdaptiveLocaleState<T extends LocaleThemeBase>
+    extends WidgetBindingState<AdaptiveLocale<T>> {
+  late T _locale = widget.adapter.adapt;
+  T get locale => _locale;
+  set locale(T value) {
+    if (value != _locale) setState(() => _locale = value);
+  }
+
+  @override
+  void didUpdateWidget(covariant AdaptiveLocale<T> oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.adapter != oldWidget.adapter) locale = widget.adapter.adapt;
+  }
+
+  @override
+  void didChangeLocales(List<Locale>? locales) {
+    super.didChangeLocales(locales);
+    if (widget.adapter.settings.isEmpty) locale = widget.adapter.adapt;
+  }
+
+  @override
+  Widget build(BuildContext context) => widget.child.localeAs(locale);
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(DiagnosticsProperty<T>('locale', locale));
+  }
+}
 
 class LocaleAdapter<T extends LocaleThemeBase> {
   const LocaleAdapter({
--- packages/auto_stories/test/state/theme_locale_test.dart
+++ packages/auto_stories/test/state/theme_locale_test.dart
@@ -29,6 +29,68 @@ void main() {
       zhHantHK,
     );
   });
+
+  testWidgets('adapt system', (t) async {
+    final binding = TestWidgetsFlutterBinding.ensureInitialized();
+    binding.platformDispatcher.localesTestValue = [];
+    final probe = Builder(
+      builder: (context) {
+        return context.find<LocaleTheme>()!.id.toString().asText();
+      },
+    );
+    const adapter = LocaleAdapter(
+      locales: [enUS, zhHansSG],
+      defaultLocale: zhHansCN,
+    );
+    await t.pumpWidget(probe.center().adaptiveLocale(adapter));
+    expect(find.text(zhHansCN.id.toString()), findsOneWidget);
+
+    binding.platformDispatcher.localesTestValue = [const Locale('en')];
+    await t.pump();
+    expect(find.text(enUS.id.toString()), findsOneWidget);
+
+    // Use first matched value rather than default value (same language).
+    binding.platformDispatcher.localesTestValue = [const Locale('zh')];
+    await t.pump();
+    expect(find.text(zhHansSG.id.toString()), findsOneWidget);
+
+    // Won't use default value unless completely necessary.
+    binding.platformDispatcher.localesTestValue = [const Locale('zh', 'CN')];
+    await t.pump();
+    expect(find.text(zhHansSG.id.toString()), findsOneWidget);
+  });
+
+  testWidgets('resolve null', (t) async {
+    final binding = TestWidgetsFlutterBinding.ensureInitialized();
+    binding.platformDispatcher.localesTestValue = [];
+    final probe = Builder(
+      builder: (context) {
+        return context.find<LocaleTheme>()!.id.toString().asText();
+      },
+    );
+    const adapter = LocaleAdapter(
+      locales: [enUS, zhHansSG],
+      defaultLocale: zhHantHK,
+    );
+    await t.pumpWidget(probe.center().adaptiveLocale(adapter));
+    expect(find.text(zhHantHK.id.toString()), findsOneWidget);
+
+    // Match first in the system locales.
+    binding.platformDispatcher.localesTestValue = [
+      const Locale('zh', 'CN'),
+      const Locale('en'),
+    ];
+    await t.pump();
+    expect(find.text(zhHansSG.id.toString()), findsOneWidget);
+
+    // Match first in the system locales regardless of area code.
+    binding.platformDispatcher.localesTestValue = [
+      const Locale('zh'),
+      const Locale('en', 'SG'),
+    ];
+    await t.pump();
+    expect(find.text(zhHansSG.id.toString()), findsOneWidget);
+  });
 }
 
 class LocaleTheme extends LocaleThemeBase {
@@ -47,4 +109,14 @@ const zhHansCN = LocaleTheme(
 const zhHantHK = LocaleTheme(
   name: '中文(繁體·中國香港)',
   id: LocaleID('zh', scriptCode: 'Hant', areaCode: 'HK'),
+);
+
+const zhHansSG = LocaleTheme(
+  name: '中文(简体·新加坡)',
+  id: LocaleID('zh', scriptCode: 'Hans', areaCode: 'SG'),
+);
+
+const enUS = LocaleTheme(
+  name: 'English (United States)',
+  id: LocaleID('en', areaCode: 'US'),
 );
