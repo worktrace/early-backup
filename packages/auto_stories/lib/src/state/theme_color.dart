6b8d4a0d5e6b59040097468b90b3875e6b2b2c0f
James Aprosail <aprosail@outlook.com>
2024-12-27 10:44:11 +0800

    feat: color theme apply

--- packages/auto_stories/lib/src/state/theme_color.dart
+++ packages/auto_stories/lib/src/state/theme_color.dart
@@ -1,5 +1,17 @@
 import 'package:auto_stories/src/utils.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
+
+import 'binding.dart';
+
+extension WrapColorTheme on Widget {
+  Widget colorThemeAs<T extends ColorThemeBase>(BuildContext context, T theme) {
+    return inherit(theme)
+        .inherit(theme.brightness)
+        .background(theme.background)
+        .maybeForegroundAs(context, theme.foreground);
+  }
+}
 
 abstract class ColorThemeBase extends AreaColors {
   const ColorThemeBase.light({
