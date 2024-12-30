1823de959a229b9a547e5b24a16c5228aa4233eb
James Aprosail <aprosail@outlook.com>
2024-12-30 11:46:36 +0800

    feat: single animation state reuse

--- packages/auto_stories/lib/src/utils/animation.dart
+++ packages/auto_stories/lib/src/utils/animation.dart
@@ -1,4 +1,57 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
+
+/// The animation [controller] had been bind with [setState] callback here.
+/// You may consider [SingleAnimationStateBare] instead.
+abstract class SingleAnimationState<S extends StatefulWidget>
+    extends SingleAnimationStateBare<S> {
+  /// Encapsulate [setState] for [dispose] to call the same one.
+  void _setState() => setState(() {});
+
+  @override
+  @mustCallSuper
+  void initState() {
+    super.initState();
+    controller.addListener(_setState);
+  }
+
+  @override
+  @mustCallSuper
+  void dispose() {
+    controller.removeListener(_setState);
+    super.dispose();
+  }
+}
+
+/// The animation [controller] had not been bind with [setState] callback here.
+/// You may consider [SingleAnimationState] instead.
+abstract class SingleAnimationStateBare<S extends StatefulWidget>
+    extends State<S> with SingleTickerProviderStateMixin {
+  late final AnimationController controller = setupController();
+
+  AnimationController setupController() => AnimationController(vsync: this);
+
+  @override
+  @mustCallSuper
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  @mustCallSuper
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  @mustCallSuper
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    final c = controller;
+    properties.add(DiagnosticsProperty<AnimationController>('controller', c));
+  }
+}
 
 extension AnimationUtils on AnimationController {
   TickerFuture animateAs(AnimationData animation, double target) {
