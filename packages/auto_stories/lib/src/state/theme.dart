b05ca21c362f8202dd02cd2829ac25af851de44b
James Aprosail <aprosail@outlook.com>
2025-01-02 10:29:59 +0800

    chore: unify theme inherit

---
+++ packages/auto_stories/lib/src/state/theme.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+import 'binding.dart';
+import 'theme_color.dart';
+import 'theme_size.dart';
+
+extension WrapTheme on Widget {
+  Widget themeAs<C extends ColorThemeBase, S extends SizeThemeBase>(
+    BuildContext context,
+    C colors,
+    S sizes,
+  ) {
+    return inherit<S>(sizes).inherit<C>(colors);
+  }
+}
