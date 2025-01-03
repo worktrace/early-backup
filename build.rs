963230b80a906b2dbebdf03e3d0f04027f8e3526
James Aprosail <aprosail@outlook.com>
2025-01-03 16:07:54 +0800

    perf: move render output and ignore

--- build.rs
+++ build.rs
@@ -6,17 +6,18 @@ fn main() {
     #[cfg(feature = "logo")]
     {
         let logo_names = ["worktrace-app", "worktrace-full"];
-        render_logo("brand", logo_names.iter()).unwrap();
+        render_logo("brand", "brand/out", logo_names.iter()).unwrap();
     }
 }
 
 fn render_logo(
-    base_path: impl AsRef<Path>,
+    src_dir: impl AsRef<Path>,
+    out_dir: impl AsRef<Path>,
     names: Iter<impl AsRef<str>>,
 ) -> Result<(), RenderLogoErr> {
     for name in names {
-        let svg_src = base_path.as_ref().join(format!("{}.svg", name.as_ref()));
-        let png_out = base_path.as_ref().join(format!("{}.png", name.as_ref()));
+        let svg_src = src_dir.as_ref().join(format!("{}.svg", name.as_ref()));
+        let png_out = out_dir.as_ref().join(format!("{}.png", name.as_ref()));
         render_svg_logo(&svg_src, &png_out)?;
     }
     Ok(())
