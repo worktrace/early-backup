fb84be9e7e01cf88dbbfd314da91fdbabb86a9a6
James Aprosail <aprosail@outlook.com>
2025-01-03 15:44:18 +0800

    feat: render logo svg into png

--- build.rs
+++ build.rs
@@ -1,4 +1,23 @@
+use std::{path::Path, slice::Iter};
+
+use worktrace_build::logo::{render_svg_logo, RenderLogoErr};
+
 fn main() {
     #[cfg(feature = "logo")]
-    {}
+    {
+        let logo_names = ["worktrace-app", "worktrace-full"];
+        render_logo("brand", logo_names.iter()).unwrap();
+    }
+}
+
+fn render_logo(
+    base_path: impl AsRef<Path>,
+    names: Iter<impl AsRef<str>>,
+) -> Result<(), RenderLogoErr> {
+    for name in names {
+        let svg_src = base_path.as_ref().join(format!("{}.svg", name.as_ref()));
+        let png_out = base_path.as_ref().join(format!("{}.png", name.as_ref()));
+        render_svg_logo(&svg_src, &png_out)?;
+    }
+    Ok(())
 }
--- crates/worktrace-build/src/logo.rs
+++ crates/worktrace-build/src/logo.rs
@@ -4,24 +4,24 @@ use png::EncodingError;
 use resvg::{render, tiny_skia::Pixmap};
 use usvg::{Options, Transform, Tree};
 
-pub fn apply_flutter_logo(
-    svg_path: impl AsRef<Path>,
-    flutter_app_root: impl AsRef<Path>,
-) -> Result<(), ApplyLogoError> {
-    let svg_data = read(svg_path)?;
+pub fn render_svg_logo(
+    svg_src_path: impl AsRef<Path>,
+    png_out_path: impl AsRef<Path>,
+) -> Result<(), RenderLogoErr> {
+    let svg_data = read(svg_src_path)?;
     let options = Options::default();
     let tree_map = Tree::from_data(&svg_data, &options)?;
     let pixmap_size = tree_map.size().to_int_size();
     let mut pixmap = Pixmap::new(pixmap_size.width(), pixmap_size.height())
-        .ok_or(ApplyLogoError::Pixmap)?;
+        .ok_or(RenderLogoErr::Pixmap)?;
 
     render(&tree_map, Transform::default(), &mut pixmap.as_mut());
-    pixmap.save_png(flutter_app_root.as_ref().join("logo.png"))?;
+    pixmap.save_png(png_out_path)?;
     Ok(())
 }
 
 #[derive(Debug, thiserror::Error)]
-pub enum ApplyLogoError {
+pub enum RenderLogoErr {
     #[error("file system error: {0}")]
     FS(#[from] std::io::Error),
 
