1d440c6b14e5c3df8fd3461209d97f5059d13d10
James Aprosail <aprosail@outlook.com>
2025-01-06 11:40:35 +0800

    feat: render svg to pngs

--- build.rs
+++ build.rs
@@ -1,25 +1,4 @@
-use std::{fs::create_dir_all, path::Path, slice::Iter};
-
-use worktrace_build::logo::{render_svg_logo, RenderLogoErr};
-
 fn main() {
     #[cfg(feature = "logo")]
-    {
-        let logo_names = ["worktrace-app", "worktrace-full"];
-        render_logo("brand", "brand/out", logo_names.iter()).unwrap();
-    }
-}
-
-fn render_logo(
-    src_dir: impl AsRef<Path>,
-    out_dir: impl AsRef<Path>,
-    names: Iter<impl AsRef<str>>,
-) -> Result<(), RenderLogoErr> {
-    create_dir_all(&out_dir)?;
-    for name in names {
-        let svg_src = src_dir.as_ref().join(format!("{}.svg", name.as_ref()));
-        let png_out = out_dir.as_ref().join(format!("{}.png", name.as_ref()));
-        render_svg_logo(&svg_src, &png_out, Some((256, 256)))?;
-    }
-    Ok(())
+    {}
 }
--- crates/worktrace-build/src/logo.rs
+++ crates/worktrace-build/src/logo.rs
@@ -1,27 +1,39 @@
-use std::{fs::read, path::Path};
+use std::{
+    fs::read,
+    path::{Path, PathBuf},
+    slice::Iter,
+};
 
 use png::EncodingError;
 use resvg::{render, tiny_skia::Pixmap};
 use usvg::{Options, Transform, Tree};
 
-pub fn render_svg_logo(
-    svg_src_path: impl AsRef<Path>,
-    png_out_path: impl AsRef<Path>,
-    size: Option<(u32, u32)>,
-) -> Result<(), RenderLogoErr> {
-    let svg_data = read(svg_src_path)?;
-    let options = Options::default();
-    let tree_map = Tree::from_data(&svg_data, &options)?;
+pub struct RenderTarget {
+    pub path: PathBuf,
+    pub size: Option<(u32, u32)>,
+}
 
-    // Pixel map size, when not specified, use svg size by default.
-    let (width, height) = size.unwrap_or_else(|| {
+impl RenderTarget {
+    pub fn from(path: impl AsRef<Path>, size: Option<(u32, u32)>) -> Self {
+        Self {
+            path: path.as_ref().into(),
+            size,
+        }
+    }
+}
+
+pub fn svg_to_png(
+    tree_map: &Tree,
+    target: &RenderTarget,
+) -> Result<(), RenderSvgErr> {
+    let (width, height) = target.size.unwrap_or_else(|| {
         let size = tree_map.size().to_int_size();
         (size.width(), size.height())
     });
-    let mut pixmap = Pixmap::new(width, height).ok_or(RenderLogoErr::Pixmap)?;
+    let mut pixmap = Pixmap::new(width, height).ok_or(RenderSvgErr::Pixmap)?;
 
     // Transform as configured size, scale rather than clip.
-    let transform = if let Some(size) = size {
+    let transform = if let Some(size) = target.size {
         let (width, height) = size;
         let svg_size = tree_map.size().to_int_size();
         let scale_x = width as f32 / svg_size.width() as f32;
@@ -32,12 +44,25 @@ pub fn render_svg_logo(
     };
 
     render(&tree_map, transform, &mut pixmap.as_mut());
-    pixmap.save_png(png_out_path)?;
+    pixmap.save_png(&target.path)?;
+    Ok(())
+}
+
+pub fn svg_to_pngs(
+    source: impl AsRef<Path>,
+    targets: Iter<RenderTarget>,
+) -> Result<(), RenderSvgErr> {
+    let data = read(source)?;
+    let options = Options::default();
+    let tree_map = Tree::from_data(&data, &options)?;
+    for target in targets {
+        svg_to_png(&tree_map, target)?;
+    }
     Ok(())
 }
 
 #[derive(Debug, thiserror::Error)]
-pub enum RenderLogoErr {
+pub enum RenderSvgErr {
     #[error("file system error: {0}")]
     FS(#[from] std::io::Error),
 
