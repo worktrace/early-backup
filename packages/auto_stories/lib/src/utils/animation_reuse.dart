49babe0cd45227e4b4a584dd7bc9a499e7a92132
James Aprosail <aprosail@outlook.com>
2025-01-07 15:46:31 +0800

    feat: handler of data update

--- packages/auto_stories/lib/src/utils/animation_reuse.dart
+++ packages/auto_stories/lib/src/utils/animation_reuse.dart
@@ -2,6 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 
 import 'animation.dart';
+import 'inherit.dart';
 
 class SingleAnimation<T> extends SingleAnimationWidget {
   const SingleAnimation({
--- packages/auto_stories/lib/src/utils/inherit.dart
+++ packages/auto_stories/lib/src/utils/inherit.dart
@@ -64,3 +64,47 @@ class InheritUpdater<T> extends InheritedWidget {
     properties.add(ObjectFlagProperty<ValueChanged<T>>.has('onChange', c));
   }
 }
+
+typedef DataBuilder<T> = Widget Function(BuildContext context, T data);
+
+class Handler<T> extends StatefulWidget {
+  const Handler({
+    super.key,
+    required this.data,
+    this.onChange,
+    required this.builder,
+  });
+
+  final T data;
+  final ValueChanged<T>? onChange;
+  final DataBuilder<T> builder;
+
+  @override
+  State<Handler<T>> createState() => _HandlerState<T>();
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties
+      ..add(ObjectFlagProperty<ValueChanged<T>?>.has('onChange', onChange))
+      ..add(DiagnosticsProperty<T>('data', data))
+      ..add(ObjectFlagProperty<DataBuilder<T>>.has('builder', builder));
+  }
+}
+
+class _HandlerState<T> extends State<Handler<T>> {
+  late T _data = widget.data;
+  void updateData(T value) {
+    if (value == _data) return;
+    setState(() => _data = value);
+    widget.onChange?.call(value);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget
+        .builder(context, _data)
+        .inherit(_data)
+        .inheritUpdater(updateData);
+  }
+}
