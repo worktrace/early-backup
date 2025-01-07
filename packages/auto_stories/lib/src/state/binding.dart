8f4dcbd6a24f295fb61e11f1353506d61060edf5
James Aprosail <aprosail@outlook.com>
2025-01-07 14:30:31 +0800

    feat: inherit updater

--- packages/auto_stories/lib/src/state/binding.dart
+++ packages/auto_stories/lib/src/state/binding.dart
@@ -3,11 +3,26 @@ import 'package:flutter/widgets.dart';
 
 extension FindContext on BuildContext {
   T? find<T>() => dependOnInheritedWidgetOfExactType<Inherit<T>>()?.data;
+  void update<T>(T data) {
+    dependOnInheritedWidgetOfExactType<InheritUpdater<T>>()?.onChange(data);
+  }
 }
 
 extension WrapInherit on Widget {
   Inherit<T> inherit<T>(T data, {Key? key}) {
-    return Inherit<T>(key: key, data: data, child: this);
+    return Inherit<T>(
+      key: key,
+      data: data,
+      child: this,
+    );
+  }
+
+  InheritUpdater<T> inheritUpdater<T>(ValueChanged<T> onChange, {Key? key}) {
+    return InheritUpdater<T>(
+      key: key,
+      onChange: onChange,
+      child: this,
+    );
   }
 }
 
@@ -25,6 +40,28 @@ class Inherit<T> extends InheritedWidget {
   void debugFillProperties(DiagnosticPropertiesBuilder properties) {
     super.debugFillProperties(properties);
     properties.add(DiagnosticsProperty<T>('data', data));
+  }
+}
+
+class InheritUpdater<T> extends InheritedWidget {
+  const InheritUpdater({
+    super.key,
+    required this.onChange,
+    required super.child,
+  });
+
+  final ValueChanged<T> onChange;
+
+  @override
+  bool updateShouldNotify(covariant InheritUpdater<T> oldWidget) {
+    return onChange != oldWidget.onChange;
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    final c = onChange;
+    properties.add(ObjectFlagProperty<ValueChanged<T>>.has('onChange', c));
   }
 }
 
