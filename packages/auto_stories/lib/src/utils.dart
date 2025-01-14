f25247cabc5245ae019903b163c14904c139c06d
James Aprosail <aprosail@outlook.com>
2025-01-14 15:40:43 +0800

    feat: golden ratio and number utils

--- packages/auto_stories/lib/src/utils.dart
+++ packages/auto_stories/lib/src/utils.dart
@@ -3,6 +3,7 @@ export 'package:bang_lerp/bang_lerp.dart';
 export 'package:bind_state/bind_state.dart';
 export 'package:inherit/inherit.dart';
 export 'package:modifier/modifier.dart';
+export 'package:syntax_sugar/syntax_sugar.dart';
 export 'package:wrap/wrap.dart';
 
 export 'utils/colors.dart';
--- packages/syntax_sugar/lib/syntax_sugar.dart
+++ packages/syntax_sugar/lib/syntax_sugar.dart
@@ -1,1 +1,1 @@
-
+export 'src/number.dart';
