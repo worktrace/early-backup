c8391c322ebb77c505778efa7770f87945e31f4e
James Aprosail <aprosail@outlook.com>
2025-01-17 11:53:08 +0800

    feat: canvas structure and fix naming

--- packages/paint_graph/lib/paint_logo.dart
+++
@@ -1,2 +0,0 @@
-export 'src/elements.dart';
-export 'src/painter.dart';
--- packages/paint_graph/lib/src/painter.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/widgets.dart';
-
-import 'elements.dart';
-
-extension PaintGraph on Canvas {
-  void paintGraph(List<GraphElement> elements) {}
-}
