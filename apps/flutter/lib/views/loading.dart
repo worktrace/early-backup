47713eeb91a09c82fb68436636e6ece801987d6d
James Aprosail <aprosail@outlook.com>
2025-01-17 09:34:37 +0800

    feat: loading route

--- apps/flutter/lib/views/loading.dart
+++ apps/flutter/lib/views/loading.dart
@@ -2,8 +2,8 @@ import 'package:auto_stories/kit.dart';
 import 'package:go_router/go_router.dart';
 import 'package:worktrace/state.dart';
 
-final welcomeRoute = GoRoute(
-  path: '/welcome',
+final loadingRoute = GoRoute(
+  path: '/',
   builder: (context, state) {
     final locale = context.find<Locale>()!;
     return '${locale.loading}...'.asText().center();
