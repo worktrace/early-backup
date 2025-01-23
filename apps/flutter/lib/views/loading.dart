20e9454a54c19f12d3421aa1e78ddf01412e5d5b
James Aprosail <aprosail@outlook.com>
2025-01-23 15:20:51 +0800

    feat: go router loading animation

--- apps/flutter/lib/views/loading.dart
+++ apps/flutter/lib/views/loading.dart
@@ -2,12 +2,14 @@ import 'package:auto_stories/kit.dart';
 import 'package:go_router/go_router.dart';
 import 'package:worktrace/state.dart';
 
+import 'welcome.dart';
+
 final loadingRoute = GoRoute(
   path: '/',
   builder: (context, state) {
     final locale = context.find<Locale>()!;
     return LoadingContainer(
-      onFinish: () {},
+      onFinish: () => context.go(welcomeRoute.path),
       onError: (err, trace) {},
       child: '${locale.loading}...'.asText().center(),
     );
