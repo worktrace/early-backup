08fa6691c36f1dd9938dd6d99dd1f40c14e3f9a4
James Aprosail <aprosail@outlook.com>
2025-01-23 09:47:02 +0800

    feat: ripple area reuse as animated mouse region

--- packages/smooth_ui/lib/src/ripple.dart
+++ packages/smooth_ui/lib/src/ripple.dart
@@ -1,5 +1,33 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
+import 'package:state_reuse/state_reuse.dart';
 import 'package:wrap/wrap.dart';
+
+const kRippleDuration = Duration(milliseconds: 245);
+const kRippleAnimation = AnimationDefibrillation(duration: kRippleDuration);
+
+abstract class RippleBase extends AnimatedMouseWidget {
+  const RippleBase({
+    super.key,
+    super.animation = kRippleAnimation,
+    this.hold = false,
+    super.onEnter,
+    super.onExit,
+    super.onHover,
+    super.opaque,
+    super.hitTestBehavior,
+    this.child,
+  });
+
+  final bool hold;
+  final Widget? child;
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(DiagnosticsProperty<bool>('hold', hold));
+  }
+}
 
 abstract class RipplePainter extends CustomPainter {
   const RipplePainter({
