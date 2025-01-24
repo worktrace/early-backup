1296966749033ac8495de8f8235548d2f742ecb5
James Aprosail <aprosail@outlook.com>
2025-01-24 11:55:26 +0800

    feat: ripple state reuse

--- packages/smooth_ui/lib/src/ripple.dart
+++ packages/smooth_ui/lib/src/ripple.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:state_reuse/state_reuse.dart';
 import 'package:wrap/wrap.dart';
@@ -31,20 +34,84 @@ abstract class RippleBase extends AnimatedMouseWidget {
 
 abstract class RippleBaseState<S extends RippleBase>
     extends SingleAnimationState<S> {
-  CustomPainter get painter;
-
   @override
-  Widget build(BuildContext context) {
-    return CustomPaint(
-      painter: painter,
-      child: widget.child,
-    );
+  @mustCallSuper
+  void initState() {
+    super.initState();
+    controller.addStatusListener(_resolveAnimationComplete);
+  }
+
+  Offset _center = Offset.zero;
+  Offset get center => _center;
+
+  bool get hover => _resolvedHover;
+  var _hover = false;
+  var _resolvedHover = false;
+  Offset _tempCenter = Offset.zero;
+
+  Future<void> _resolveAnimationComplete(AnimationStatus status) async {
+    if (status == AnimationStatus.completed && !hover) {
+      await _removeRippleAnim();
+    }
+  }
+
+  Future<void> _removeRippleAnim() {
+    setState(() => _center = _tempCenter);
+    return controller.animateToStart(widget.animation);
+  }
+
+  Future<void> _mouseEnter(PointerEnterEvent event) async {
+    _hover = true;
+    await Future<void>.delayed(widget.animation.defibrillation);
+    if (!_hover || !mounted) return;
+
+    _resolvedHover = true;
+    widget.onEnter?.call(event);
+    setState(() => _center = event.localPosition);
+    return controller.animateToEnd(widget.animation);
+  }
+
+  Future<void> _mouseExit(PointerExitEvent event) async {
+    _hover = false;
+    await Future<void>.delayed(widget.animation.defibrillation);
+    if (_hover || !mounted) return;
+
+    _resolvedHover = false;
+    widget.onExit?.call(event);
+    if (controller.isCompleted && !widget.hold) {
+      setState(() => _center = event.localPosition);
+      return controller.animateToStart(widget.animation);
+    }
+    _tempCenter = event.localPosition;
   }
 
   @override
+  void didUpdateWidget(covariant S oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.hold && !widget.hold) unawaited(_removeRippleAnim());
+  }
+
+  CustomPainter get painter;
+
+  @override
+  @mustCallSuper
+  Widget build(BuildContext context) =>
+      widget.child.paint(painter: painter).mouse(
+            onEnter: _mouseEnter,
+            onExit: _mouseExit,
+            onHover: widget.onHover,
+            opaque: widget.opaque,
+            hitTestBehavior: widget.hitTestBehavior,
+          );
+
+  @override
+  @mustCallSuper
   void debugFillProperties(DiagnosticPropertiesBuilder properties) {
     super.debugFillProperties(properties);
-    properties.add(DiagnosticsProperty<CustomPainter>('painter', painter));
+    properties
+      ..add(DiagnosticsProperty<Offset>('center', center))
+      ..add(DiagnosticsProperty<bool>('hover', hover))
+      ..add(DiagnosticsProperty<CustomPainter>('painter', painter));
   }
 }
 
