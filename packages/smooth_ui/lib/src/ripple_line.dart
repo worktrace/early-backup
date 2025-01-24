c3889787e52602dab062180bc1211351a365b89b
James Aprosail <aprosail@outlook.com>
2025-01-24 16:17:30 +0800

    feat: compute line area rect

--- packages/smooth_ui/lib/src/ripple_line.dart
+++ packages/smooth_ui/lib/src/ripple_line.dart
@@ -1,1 +1,31 @@
+import 'dart:math';
 
+import 'package:flutter/widgets.dart';
+
+Rect lineAreaOf(Size size, EdgeInsets padding, Offset center, double ratio) {
+  return size.width > size.height
+      ? _hArea(size, padding, center, ratio)
+      : _vArea(size, padding, center, ratio);
+}
+
+Rect _hArea(Size size, EdgeInsets padding, Offset center, double ratio) {
+  final c = center.dx;
+  final radius = size.width * ratio;
+  return Rect.fromLTRB(
+    max(c - radius, padding.left),
+    padding.top,
+    min(c + radius, size.width - padding.right),
+    size.height - padding.bottom,
+  );
+}
+
+Rect _vArea(Size size, EdgeInsets padding, Offset center, double ratio) {
+  final c = center.dy;
+  final radius = size.height * ratio;
+  return Rect.fromLTRB(
+    padding.left,
+    max(c - radius, padding.top),
+    size.width - padding.right,
+    min(c + radius, size.height - padding.bottom),
+  );
+}
