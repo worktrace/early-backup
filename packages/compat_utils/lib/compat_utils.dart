b3dd12181f0724b20f7251424d765ebb68d1599e
James Aprosail <aprosail@outlook.com>
2025-02-06 14:29:08 +0800

    chore: prepare namespace in multi files

--- packages/compat_utils/lib/compat_utils.dart
+++ packages/compat_utils/lib/compat_utils.dart
@@ -1,5 +1,6 @@
 export 'src/files.dart';
 export 'src/format.dart';
 export 'src/number.dart';
-export 'src/terminal.dart';
+export 'src/terminal_decorate.dart';
+export 'src/terminal_wrap.dart';
 export 'src/types.dart';
