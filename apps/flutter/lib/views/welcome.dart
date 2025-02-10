befc8886d55646d2143c4f40387c01eb9eecbc24
James Aprosail <aprosail@outlook.com>
2025-02-10 16:40:47 +0800

    chore: apply sidebar for testing

--- apps/flutter/lib/views/welcome.dart
+++ apps/flutter/lib/views/welcome.dart
@@ -1,21 +1,12 @@
 import 'package:auto_stories/kit.dart';
-import 'package:flutter/widgets.dart';
 import 'package:go_router/go_router.dart';
 
 final welcomeRoute = GoRoute(
   path: '/welcome',
-  pageBuilder: (context, state) {
-    return CustomTransitionPage(
-      child: 'welcome'.asText().center(),
-      transitionsBuilder: (context, animation, secondaryAnimation, child) {
-        return SlideTransition(
-          position: Tween(
-            begin: const Offset(1, 0),
-            end: Offset.zero,
-          ).animate(animation),
-          child: child,
-        );
-      },
+  builder: (context, state) {
+    return SidebarContainer(
+      sidebar: 'sidebar'.asText().center(),
+      child: 'content'.asText().center(),
     );
   },
 );
