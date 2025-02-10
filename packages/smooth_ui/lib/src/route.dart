d631ba015dc2aceb93a5c10b73a02186a538db07
James Aprosail <aprosail@outlook.com>
2025-02-10 14:38:24 +0800

    feat: bind route animation builder parameter

--- packages/smooth_ui/lib/src/route.dart
+++ packages/smooth_ui/lib/src/route.dart
@@ -1,17 +1,35 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:state_reuse/state_reuse.dart';
+
+typedef RouteAnimationBuilder = Widget Function(
+  double value,
+  Widget child,
+  Widget oldChild,
+);
 
 class RouteContainer extends SingleAnimationWidget {
   const RouteContainer({
     super.key,
     super.animation,
+    required this.animationBuilder,
     required this.child,
   });
 
+  final RouteAnimationBuilder animationBuilder;
   final Widget child;
 
   @override
   State<RouteContainer> createState() => _RouteContainerState();
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    final a = animationBuilder;
+    properties.add(
+      ObjectFlagProperty<RouteAnimationBuilder>.has('animationBuilder', a),
+    );
+  }
 }
 
 class _RouteContainerState extends SingleAnimationState<RouteContainer> {
