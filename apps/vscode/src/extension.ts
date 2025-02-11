393ea4bad84d33f085d33467c770ea1ff591ee46
James Aprosail <aprosail@outlook.com>
2025-02-11 14:30:16 +0800

    chore: vscode hello as placeholder

--- apps/vscode/src/extension.ts
+++ apps/vscode/src/extension.ts
@@ -1,5 +1,9 @@
 import * as vscode from "vscode"
 
 export function activate(context: vscode.ExtensionContext) {
-  context.subscriptions.push()
+  context.subscriptions.push(
+    vscode.commands.registerCommand("worktrace.hello", () => {
+      vscode.window.showInformationMessage("Hello from WorkTrace!")
+    }),
+  )
 }
