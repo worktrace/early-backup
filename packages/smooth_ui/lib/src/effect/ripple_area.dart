3419affe5b67865200a13cd85b6b502e9f1e92f3
James Aprosail <aprosail@outlook.com>
2025-02-17 11:46:40 +0800

    perf: nested file structure for smooth ui package

--- packages/smooth_ui/lib/src/effect/ripple_area.dart
+++ packages/smooth_ui/lib/src/effect/ripple_area.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
+import 'package:smooth_ui/src/utils.dart';
 import 'package:state_reuse/state_reuse.dart';
 import 'package:wrap/wrap.dart';
 
 import 'ripple.dart';
-import 'utils.dart';
 
 extension WrapRippleArea on Widget? {
   RippleArea ripple({
