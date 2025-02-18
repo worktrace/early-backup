7c232f597202cdeebfddb2366b04ff65817d7743
James Aprosail <aprosail@outlook.com>
2025-02-18 11:25:21 +0800

    feat: apply sidebar theme and background

--- apps/flutter/lib/views/welcome.dart
+++ apps/flutter/lib/views/welcome.dart
@@ -1,10 +1,13 @@
 import 'package:auto_stories/kit.dart';
 import 'package:go_router/go_router.dart';
+import 'package:worktrace/state.dart';
 
 final welcomeRoute = GoRoute(
   path: '/welcome',
   builder: (context, state) {
+    final theme = context.find<Theme>()!;
     return SidebarContainer(
+      colors: theme.sidebar,
       sidebar: 'sidebar'.asText().center(),
       child: 'content'.asText().center(),
     );
