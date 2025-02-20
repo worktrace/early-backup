1fa838499c085b8ef9e2ace8f1024385bafebc9f
James Aprosail <aprosail@outlook.com>
2025-02-20 10:16:54 +0800

    feat: wrap opacity

--- packages/wrap/lib/src/decorate.dart
+++ packages/wrap/lib/src/decorate.dart
@@ -31,6 +31,19 @@ extension WrapDecoration on Widget {
 }
 
 extension WrapFilter on Widget? {
+  Opacity opacity(
+    double opacity, {
+    Key? key,
+    bool alwaysIncludeSemantics = false,
+  }) {
+    return Opacity(
+      key: key,
+      opacity: opacity,
+      alwaysIncludeSemantics: alwaysIncludeSemantics,
+      child: this,
+    );
+  }
+
   BackdropFilter filter({
     Key? key,
     required ImageFilter filter,
