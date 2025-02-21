2e5f3d1da0488b73b3309f9d8eb2a00ff724926a
James Aprosail <aprosail@outlook.com>
2025-02-21 14:42:34 +0800

    fix: parse annotation abstraction adapt source gen

--- packages/data_gen/lib/src/parse.dart
+++ packages/data_gen/lib/src/parse.dart
@@ -1,15 +1,9 @@
-import 'package:analyzer/dart/ast/ast.dart';
+import 'package:source_gen/source_gen.dart';
 
 import 'annotation.dart';
 
-extension ParseDataAnnotation on Annotation {
+extension ParseDataAnnotation on ConstantReader {
   DataAnnotation? get parse {
-    final libraryIdentifier = element2?.library2?.identifier;
-    if (libraryIdentifier == AnnotationLibraries.map) {}
     return null;
   }
-}
-
-extension AnnotationLibraries on DataAnnotation {
-  static const map = 'package:data_anno/src/map.dart';
 }
