680b9ef53670b0de166dea9c2c83dfc53a25738a
James Aprosail <aprosail@outlook.com>
2025-02-21 14:23:20 +0800

    docs: details about code count

--- scripts/count-lines.sh
+++ scripts/count-lines.sh
@@ -1,5 +1,10 @@
 # Require tokei to be installed.
 #
+# 1. Don't count code inside rust comment docs.
+# 2. Count code in hidden files.
+# 3. Sort by lines of code of each language.
+# 4. Don't count configuration files.
+#
 # docs: https://github.com/XAMPPRocky/tokei?tab=readme-ov-file#installation
 # source code: https://github.com/XAMPPRocky/tokei
 # cargo install: https://crates.io/crates/tokei
