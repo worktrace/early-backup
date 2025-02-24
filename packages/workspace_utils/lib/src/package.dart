57c198708242ee2c9871bf286987fafc58324100
James Aprosail <aprosail@outlook.com>
2025-02-24 11:54:51 +0800

    feat: commonly used directories

--- packages/workspace_utils/lib/src/package.dart
+++ packages/workspace_utils/lib/src/package.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:compat_utils/compat_utils.dart';
+import 'package:path/path.dart';
 
 class DartPackage {
   const DartPackage(this.root);
@@ -12,4 +13,9 @@ class DartPackage {
   }
 
   final Directory root;
+
+  Directory get binDir => Directory(join(root.path, 'bin'));
+  Directory get libDir => Directory(join(root.path, 'lib'));
+  Directory get testDir => Directory(join(root.path, 'test'));
+  Directory get exampleDir => Directory(join(root.path, 'example'));
 }
