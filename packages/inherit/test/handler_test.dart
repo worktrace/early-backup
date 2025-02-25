d8a3fc258512e828e8cffe3dd440955b47dfe0ab
James Aprosail <aprosail@outlook.com>
2025-02-25 16:55:43 +0800

    test: handler update

--- packages/inherit/test/handler_test.dart
+++ packages/inherit/test/handler_test.dart
@@ -1,23 +1,56 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:inherit/debug.dart';
+import 'package:inherit/inherit.dart';
+import 'package:modifier/modifier.dart';
 import 'package:wrap/debug.dart';
+import 'package:wrap/wrap.dart';
 
 void main() {
   testWidgets('handler update', (t) async {
-    await t.pumpWidget(const HandlerUpdate().ensureText());
+    const before = 'before';
+    const after = 'after';
+    const button = 'button';
+    const widget = HandlerUpdate(
+      before: before,
+      after: after,
+      button: button,
+    );
+    await t.pumpWidget(widget.ensureText());
+    expect(find.text(before), findsOneWidget);
+    expect(find.text(button), findsOneWidget);
+
+    await t.tap(find.text(button));
+    await t.pump();
+    expect(find.text(after), findsOneWidget);
   });
 }
 
-class HandlerUpdate extends StatefulWidget {
-  const HandlerUpdate({super.key});
+class HandlerUpdate extends UpdateTester {
+  const HandlerUpdate({
+    super.key,
+    required super.before,
+    required super.after,
+    required super.button,
+  });
 
   @override
   State<HandlerUpdate> createState() => _HandlerUpdateState();
 }
 
-class _HandlerUpdateState extends State<HandlerUpdate> {
+class _HandlerUpdateState extends UpdateTesterState<HandlerUpdate> {
+  @override
+  Widget buttonBuilder(BuildContext context) {
+    return widget.button
+        .asText()
+        .gesture(onTap: () => context.update(widget.after));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Handler(
+      data: message,
+      builder: (context, data) => [data.asText(), button].asColumn().center,
+    );
   }
 }
