56d1dca4b29744e660eb694cfbaf1dee8c823e2d
James Aprosail <aprosail@outlook.com>
2025-02-25 10:49:29 +0800

    chore: animated painter parameters

--- packages/smooth_ui/lib/src/icons/animated_icon.dart
+++ packages/smooth_ui/lib/src/icons/animated_icon.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/widgets.dart';
 
+class AnimatedIcon extends StatefulWidget {
+  const AnimatedIcon({super.key});
+
+  @override
+  State<AnimatedIcon> createState() => _AnimatedIconState();
+}
+
+class _AnimatedIconState extends State<AnimatedIcon> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
