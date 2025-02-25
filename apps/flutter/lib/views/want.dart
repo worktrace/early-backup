4d8b1b8822cb8b6aa319902d042d0d5e33e76831
James Aprosail <aprosail@outlook.com>
2025-02-25 14:04:45 +0800

    fix: add missing theme binding

--- apps/flutter/lib/views/want.dart
+++ apps/flutter/lib/views/want.dart
@@ -1,12 +1,15 @@
 import 'package:auto_stories/kit.dart';
 import 'package:go_router/go_router.dart';
+import 'package:worktrace/state.dart';
 
 import 'sidebar.dart';
 
 final wantRoute = GoRoute(
   path: '/want',
   builder: (context, state) {
+    final theme = context.find<Theme>()!;
     return SidebarContainer(
+      colors: theme.sidebar,
       sidebar: const Sidebar(),
       child: 'create want'.asText().center(),
     );
