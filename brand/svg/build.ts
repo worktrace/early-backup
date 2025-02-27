a390f1f3f6e8ff447649b1a2006adcb0a7adcc9f
James Aprosail <aprosail@outlook.com>
2025-02-27 16:59:04 +0800

    perf: use lit to define xml

--- brand/svg/build.ts
+++ brand/svg/build.ts
@@ -1,2 +1,9 @@
-function main() {}
+import { renderLogo } from "./src/graph"
+
+function main() {
+  const full = renderLogo()
+
+  // oxlint-disable-next-line no-console
+  console.log(full)
+}
 main()
--- brand/svg/src/graph.ts
+++ brand/svg/src/graph.ts
@@ -1,1 +1,17 @@
-export function graph() {}
+import { html } from "lit"
+
+export function renderLogo(): string {
+  const xmlHeader = `<?xml version="1.0"?>`
+  const dom = html`
+    <svg>
+      <g id="logo">
+        <rect></rect>
+      </g>
+    </svg>
+  `
+
+  return `${xmlHeader}${dom.strings[0]}`
+    .split("\n")
+    .map((line) => line.trim())
+    .join("")
+}
