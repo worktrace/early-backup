662613477e8c4269dcf7063bf468cea2db1549c1
James Aprosail <aprosail@outlook.com>
2025-02-27 09:35:39 +0800

    feat: command line encapsulation for test all

--- packages/workspace_utils/bin/workspace_utils.dart
+++ packages/workspace_utils/bin/workspace_utils.dart
@@ -1,5 +1,27 @@
-import 'dart:io';
+import 'dart:async';
 
-void main(List<String> arguments) {
-  stdout.writeln('it works');
+import 'package:args/command_runner.dart';
+import 'package:workspace_utils/workspace_utils.dart';
+
+Future<void> main(List<String> arguments) async {
+  const name = 'workspace_utils';
+  const description = 'Dart workspace multiple packages operations.';
+  final runner = CommandRunner<void>(name, description)
+    ..addCommand(TestCommand());
+
+  return runner.run(arguments);
+}
+
+class TestCommand extends Command<void> {
+  @override
+  String get name => 'test';
+
+  @override
+  String get description => 'Test all packages inside the root workspace.';
+
+  @override
+  Future<void> run() async {
+    final package = DartPackage.resolve();
+    await package.test();
+  }
 }
