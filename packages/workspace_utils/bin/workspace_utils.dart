3f30203f4b462b68d53b7c0a5d5b2d04cea65ce3
James Aprosail <aprosail@outlook.com>
2025-03-06 09:19:43 +0800

    feat: add build command

--- packages/workspace_utils/bin/workspace_utils.dart
+++ packages/workspace_utils/bin/workspace_utils.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:args/args.dart';
 import 'package:args/command_runner.dart';
 import 'package:workspace_utils/workspace_utils.dart';
 
@@ -7,18 +8,25 @@ Future<void> main(List<String> arguments) async {
   const name = 'workspace_utils';
   const description = 'Dart workspace multiple packages operations.';
   final runner = CommandRunner<void>(name, description)
-    ..addCommand(TestCommand());
+    ..addCommand(TestCommand())
+    ..addCommand(BuildCommand());
 
   return runner.run(arguments);
 }
 
+// Shared root option.
+const rootOptionName = 'root';
+void addRootOption(ArgParser argParser) {
+  argParser.addOption(
+    rootOptionName,
+    abbr: rootOptionName.substring(0, 1).toLowerCase(),
+    help: 'Specify the root directory where workspace root locates.',
+  );
+}
+
 class TestCommand extends Command<void> {
-  TestCommand() {
-    argParser.addOption(
-      rootOption,
-      abbr: rootOption.substring(0, 1).toLowerCase(),
-      help: 'Specify the root directory where workspace root locates.',
-    );
+  TestCommand() : super() {
+    addRootOption(argParser);
   }
 
   @override
@@ -27,12 +35,29 @@ class TestCommand extends Command<void> {
   @override
   String get description => 'Test all packages inside the root workspace.';
 
-  static const rootOption = 'root';
+  @override
+  Future<void> run() async {
+    final root = argResults?.option(rootOptionName);
+    final package = DartPackage.resolve(path: root ?? '');
+    await package.test();
+  }
+}
+
+class BuildCommand extends Command<void> {
+  BuildCommand() : super() {
+    addRootOption(argParser);
+  }
+
+  @override
+  String get name => 'build';
+
+  @override
+  String get description => 'Build all packages inside the root workspace.';
 
   @override
   Future<void> run() async {
-    final root = argResults?.option(rootOption);
+    final root = argResults?.option(rootOptionName);
     final package = DartPackage.resolve(path: root ?? '');
-    await package.test();
+    await package.build();
   }
 }
