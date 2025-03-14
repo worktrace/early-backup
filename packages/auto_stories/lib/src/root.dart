b420a56d30fc3ba8afc2ed11b943351fe057ff20
James Aprosail <aprosail@outlook.com>
2025-03-14 09:37:43 +0800

    feat: auto stories entry

---
+++ packages/auto_stories/lib/src/root.dart
@@ -0,0 +1,16 @@
+import 'package:auto_stories/kit.dart';
+import 'package:flutter/widgets.dart';
+
+void autoStoriesRoot() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const AutoStoriesRoot());
+}
+
+class AutoStoriesRoot extends StatelessWidget {
+  const AutoStoriesRoot({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return 'auto stories'.asText().center();
+  }
+}
