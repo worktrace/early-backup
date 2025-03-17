88a604696c02da1b32a77bd5e0011c238edc8c35
James Aprosail <aprosail@outlook.com>
2025-03-17 11:14:34 +0800

    feat: define window mode enum

--- packages/smooth_ui/lib/layout.dart
+++ packages/smooth_ui/lib/layout.dart
@@ -1,4 +1,5 @@
 export 'src/layout/loading.dart';
 export 'src/layout/route.dart';
 export 'src/layout/sidebar.dart';
+export 'src/layout/window_mode.dart';
 export 'utils.dart';
---
+++ packages/smooth_ui/lib/src/layout/window_mode.dart
@@ -0,0 +1,1 @@
+enum WindowMode { landscape, medium, portrait }
