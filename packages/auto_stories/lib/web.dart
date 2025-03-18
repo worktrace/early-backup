d12762f5a7ecf67c062e4ecf1f9a7d8857a060f7
James Aprosail <aprosail@outlook.com>
2025-03-18 14:10:39 +0800

    fix: upgraded conditional import and docs

--- packages/auto_stories/lib/web.dart
+++ packages/auto_stories/lib/web.dart
@@ -1,1 +1,4 @@
-export 'src/web/route_io.dart' if (dart.library.html) 'src/web/route.dart';
+// see https://dart.dev/interop/js-interop/package-web#conditional-imports
+export 'src/web/route.dart'
+    if (dart.library.io) 'src/web/route_io.dart'
+    if (dart.library.js_interop) 'src/web/route_web.dart';
