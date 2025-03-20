0e9626fa481b21f4340c3a22cb1469f66e435c54
James Aprosail <aprosail@outlook.com>
2025-03-20 10:24:33 +0800

    perf: move optimize rename

--- packages/smooth_ui/lib/layout.dart
+++ packages/smooth_ui/lib/layout.dart
@@ -1,6 +1,6 @@
 export 'src/layout/adapt_layout.dart';
-export 'src/layout/adaptive.dart';
 export 'src/layout/adaptive_data.dart';
+export 'src/layout/adaptive_layout.dart';
 export 'src/layout/loading.dart';
 export 'src/layout/route.dart';
 export 'src/layout/sidebar.dart';
