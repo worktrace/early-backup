54c60d9c489fda86371c7ee13b83fbead06cfafd
James Aprosail <aprosail@outlook.com>
2025-03-20 14:29:29 +0800

    feat: sort dependencies

--- packages/compat_utils/lib/src/sort.dart
+++ packages/compat_utils/lib/src/sort.dart
@@ -1,19 +1,37 @@
+import 'dart:collection';
+
 /// Sort [nodes] according to their dependencies,
-/// that all dependencies of a node must be sorted before the node itself.
+/// that all dependencies of a node must be sorted after the node itself.
 /// Once there's a cycle in the dependencies graph, an exception will be thrown.
 List<String> sortDependencies(Map<String, Set<String>> nodes) {
-  return [];
+  final queue = Queue<String>();
+  final sorted = <String>[];
+  final inDegrees = calculateInDegrees(nodes)..forEach((node, inDegree) {
+    if (inDegree == 0) queue.addLast(node);
+  });
+
+  while (queue.isNotEmpty) {
+    final current = queue.removeFirst();
+    sorted.add(current);
+    for (final neighbor in nodes[current]!) {
+      inDegrees[neighbor] = inDegrees[neighbor]! - 1;
+      if (inDegrees[neighbor] == 0) queue.addLast(neighbor);
+    }
+  }
+
+  if (sorted.length != nodes.length) throw Exception('cycle dependencies');
+  return sorted;
 }
 
 /// Calculate in-degree for each node in the dependencies graph.
 /// Usually called for [sortDependencies].
 Map<String, int> calculateInDegrees(Map<String, Set<String>> nodes) {
-  final buffer = <String, int>{};
-  for (final node in nodes.keys) buffer[node] = 0;
+  final inDegrees = <String, int>{};
+  for (final node in nodes.keys) inDegrees[node] = 0;
   for (final dependencies in nodes.values) {
     for (final dependency in dependencies) {
-      buffer[dependency] = (buffer[dependency] ?? 0) + 1;
+      inDegrees[dependency] = (inDegrees[dependency] ?? 0) + 1;
     }
   }
-  return buffer;
+  return inDegrees;
 }
--- packages/compat_utils/test/sort_test.dart
+++ packages/compat_utils/test/sort_test.dart
@@ -11,4 +11,24 @@ void main() {
     final inDegrees = calculateInDegrees(dependencies);
     expect(inDegrees, {'a': 0, 'b': 1, 'c': 2});
   });
+
+  test('sort dependencies', () {
+    final dependencies = <String, Set<String>>{
+      'a': {'b', 'c'},
+      'b': {'c'},
+      'c': {},
+    };
+    final sorted = sortDependencies(dependencies);
+    expect(sorted, ['a', 'b', 'c']);
+  });
+
+  test('sort dependencies cycle exception', () {
+    final dependencies = <String, Set<String>>{
+      'a': {'b'},
+      'b': {'c'},
+      'c': {'a'},
+      'd': {},
+    };
+    expect(() => sortDependencies(dependencies), throwsException);
+  });
 }
