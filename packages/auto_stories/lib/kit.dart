23f8dd32dd261d3a20e930b7d4cfd2a0f6163c1f
James Aprosail <aprosail@outlook.com>
2025-03-24 16:58:42 +0800

    fix: missing export updates

--- packages/auto_stories/lib/kit.dart
+++ packages/auto_stories/lib/kit.dart
@@ -2,6 +2,7 @@
 library;
 
 export 'package:avoid_nullable/avoid_nullable.dart';
+export 'package:compat_utils/compat_utils.dart';
 export 'package:custom_app/app.dart';
 export 'package:inherit/inherit.dart';
 export 'package:modifier/modifier.dart';
@@ -10,6 +11,5 @@ export 'package:smooth_ui/icons.dart';
 export 'package:smooth_ui/layout.dart';
 export 'package:smooth_ui/utils.dart';
 export 'package:state_reuse/state_reuse.dart';
-export 'package:syntax_sugar/syntax_sugar.dart';
 export 'package:wrap/utils.dart';
 export 'package:wrap/wrap.dart';
