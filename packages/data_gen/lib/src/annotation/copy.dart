053f7ddf442503a6efdf0a2b102dc8ba59559d27
James Aprosail <aprosail@outlook.com>
2025-03-27 16:00:54 +0800

    docs: generate strategy

--- packages/data_gen/lib/src/annotation/copy.dart
+++ packages/data_gen/lib/src/annotation/copy.dart
@@ -5,10 +5,16 @@ import 'name.dart';
 
 part 'copy.name.dart';
 
+/// Generate the `copyWith` method based on current constructor.
+///
+/// 1. Only parameters with the field of the same name will be included,
+/// 2. When there's any required parameter without corresponding field,
+/// it will throw exception when building (generating code).
 @name
 const copy = GenerateCopy();
 const String copyAnnotationName = _$copy$topLevelVariableName;
 
+/// See the [copy] annotation.
 @Target({TargetKind.constructor})
 @name
 class GenerateCopy extends DataAnnotation {
