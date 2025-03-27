2bcac1eefa0accc24368a0c1fc1e34318e50d4f6
James Aprosail <aprosail@outlook.com>
2025-03-27 16:18:22 +0800

    perf: simplify lib structure

--- packages/data_gen/lib/parse.dart
+++ packages/data_gen/lib/parse.dart
@@ -1,1 +1,9 @@
-export 'src/parse.dart';
+import 'package:data_gen/annotation.dart';
+import 'package:source_gen/source_gen.dart';
+
+extension ParseDataAnnotation on ConstantReader {
+  AnnotationToParse? get parse {
+    switch (objectValue.type?.element3?.library2?.identifier) {}
+    return null;
+  }
+}
--- packages/data_gen/lib/src/parse.dart
+++
@@ -1,9 +0,0 @@
-import 'package:data_gen/annotation.dart';
-import 'package:source_gen/source_gen.dart';
-
-extension ParseDataAnnotation on ConstantReader {
-  AnnotationToParse? get parse {
-    switch (objectValue.type?.element3?.library2?.identifier) {}
-    return null;
-  }
-}
