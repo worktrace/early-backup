af77133ab5aadd9eb68d21c21c41cb19d01257eb
James Aprosail <aprosail@outlook.com>
2025-03-28 11:29:32 +0800

    fix: element for annotation

--- packages/data_build/lib/src/builder/builder.dart
+++ packages/data_build/lib/src/builder/builder.dart
@@ -1,4 +1,5 @@
 import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/dart/element/element2.dart';
 
 typedef FileBuilder = BuildOutput? Function(String path, CompilationUnit unit);
 typedef ContentBuilder = String? Function(String path, CompilationUnit unit);
@@ -10,5 +11,5 @@ class BuildOutput {
   final String content;
 }
 
-/// How to build based on an annotation.
-typedef AnnotationBuilder<T> = String Function(T annotation);
+/// How to build based on an annotation and its annotated element.
+typedef AnnotationBuilder<T> = String Function(Element2 element, T annotation);
