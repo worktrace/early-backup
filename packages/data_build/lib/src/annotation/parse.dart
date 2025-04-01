515f6cf9e0162a70f105f272ccf67143cb44104f
James Aprosail <aprosail@outlook.com>
2025-04-01 14:12:17 +0800

    fix: avoid loop generate and provide example

--- packages/data_build/lib/src/annotation/parse.dart
+++ packages/data_build/lib/src/annotation/parse.dart
@@ -1,10 +1,8 @@
 import 'package:meta/meta_meta.dart';
 
-@parse
 const parse = GenerateParse();
 
 @Target({TargetKind.constructor, TargetKind.topLevelVariable})
 class GenerateParse {
-  @parse
   const GenerateParse();
 }
