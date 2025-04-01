37a42362b3ac9eda0c011f20637e6132d54c9c4f
James Aprosail <aprosail@outlook.com>
2025-04-01 15:10:03 +0800

    feat: wrap path utils

--- packages/compat_utils/lib/path.dart
+++ packages/compat_utils/lib/path.dart
@@ -1,7 +1,17 @@
 import 'dart:io';
 
-import 'package:path/path.dart';
+import 'package:path/path.dart' as path;
 
 extension DirectoryUtils on Directory {
-  Directory get normalized => Directory(normalize(path));
+  Directory get normalized => Directory(path.normalize(this.path));
+}
+
+extension WrapPath on String {
+  String get normalized => path.normalize(this);
+  String get relative => path.relative(this);
+  String get absolute => path.absolute(this);
+  String get basename => path.basename(this);
+  String get dirname => path.dirname(this);
+
+  String join(String another) => path.join(this, another);
 }
