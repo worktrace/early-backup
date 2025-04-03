3d234a7c8d53421df998f8b4f03aa6abfa7c68d3
James Aprosail <aprosail@outlook.com>
2025-04-03 16:14:48 +0800

    feat: animation state reuse with mixin

--- packages/state_reuse/lib/src/animation/animation_reuse.dart
+++ packages/state_reuse/lib/src/animation/animation_reuse.dart
@@ -21,7 +21,17 @@ abstract class SingleAnimationWidget extends StatefulWidget {
 /// The animation [controller] had been bind with [setState] callback here.
 /// You may consider [SingleAnimationStateBare] instead.
 abstract class SingleAnimationState<S extends StatefulWidget>
-    extends SingleAnimationStateBare<S> {
+    extends SingleAnimationStateBare<S>
+    with SingleAnimationDefault {}
+
+/// The animation [controller] had not been bind with [setState] callback here.
+/// You may consider [SingleAnimationState] instead.
+abstract class SingleAnimationStateBare<S extends StatefulWidget>
+    extends State<S>
+    with SingleTickerProviderStateMixin, SingleAnimationMixin {}
+
+mixin SingleAnimationDefault<S extends StatefulWidget>
+    on SingleAnimationMixin<S> {
   /// Encapsulate [setState] for [dispose] to call the same one.
   void _setState() => setState(() {});
 
@@ -40,11 +50,8 @@ abstract class SingleAnimationState<S extends StatefulWidget>
   }
 }
 
-/// The animation [controller] had not been bind with [setState] callback here.
-/// You may consider [SingleAnimationState] instead.
-abstract class SingleAnimationStateBare<S extends StatefulWidget>
-    extends State<S>
-    with SingleTickerProviderStateMixin {
+mixin SingleAnimationMixin<S extends StatefulWidget>
+    on SingleTickerProviderStateMixin<S> {
   late final AnimationController controller = setupController();
 
   AnimationController setupController() => AnimationController(vsync: this);
