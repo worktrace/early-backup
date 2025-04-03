4617af948860f4e3ee58f1b7841788587e900845
James Aprosail <aprosail@outlook.com>
2025-04-03 15:42:16 +0800

    chore: animated hover basic

--- packages/state_reuse/lib/src/interact/hover.dart
+++ packages/state_reuse/lib/src/interact/hover.dart
@@ -106,3 +106,36 @@ class _HoverDefibrillationState
     hitTestBehavior: widget.hitTestBehavior,
   );
 }
+
+class AnimatedHover extends MouseWidget {
+  const AnimatedHover({
+    super.key,
+    this.animation = const AnimationDefibrillation(),
+    super.onEnter,
+    super.onExit,
+    super.onHover,
+    super.cursor,
+    super.hitTestBehavior,
+    super.opaque,
+    super.child,
+  });
+
+  final AnimationDefibrillation animation;
+
+  @override
+  State<AnimatedHover> createState() => _AnimatedHoverState();
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    final a = animation;
+    properties.add(DiagnosticsProperty<AnimationDefibrillation>('anim', a));
+  }
+}
+
+class _AnimatedHoverState extends State<AnimatedHover> {
+  @override
+  Widget build(BuildContext context) {
+    return widget.child.mouse();
+  }
+}
