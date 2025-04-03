aa1445063eb60aeaa11eea900033439e62082f50
James Aprosail <aprosail@outlook.com>
2025-04-03 09:15:14 +0800

    feat: nullable iterable reuse

---
+++ packages/compat_utils/lib/iterable.dart
@@ -0,0 +1,10 @@
+extension NullableIterable<T> on Iterable<T> {
+  T? firstWhereOrNull(bool Function(T element) test) {
+    for (final element in this) if (test(element)) return element;
+    return null;
+  }
+
+  /// Usually used on dynamic types, when [whereType] is not convenient.
+  T get firstNotNull => firstWhere((i) => i != null);
+  T? get firstMaybeNotNull => firstWhereOrNull((i) => i != null);
+}
