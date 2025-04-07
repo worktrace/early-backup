e187bac094a5d712c8eb06dd6574f0cf7f6ba093
James Aprosail <aprosail@outlook.com>
2025-04-07 16:19:10 +0800

    chore: record parse fatal

--- packages/data_build/lib/src/annotation/parse.dart
+++ packages/data_build/lib/src/annotation/parse.dart
@@ -5,4 +5,9 @@ const parse = GenerateParse();
 @Target({TargetKind.constructor, TargetKind.topLevelVariable})
 class GenerateParse {
   const GenerateParse();
+
+  // Maintained manually, don't forget to update.
+  static const name = 'GenerateParse';
+  static const shortcut = 'parse';
+  static const libIdentifier = 'package:data_build/src/annotation/parse.dart';
 }
