b8ef368e0fc306d077e37e00d12da1d905ca8015
James Aprosail <aprosail@outlook.com>
2025-04-08 11:58:40 +0800

    perf: optimize code lib structure

---
+++ packages/data_build/lib/src/generator/copy.dart
@@ -0,0 +1,11 @@
+import 'package:compat_utils/types.dart';
+import 'package:source_gen/source_gen.dart';
+
+import 'abstraction.dart';
+
+final copyGenerator = AnnotationGenerator(
+  typeChecker: const TypeChecker.fromRuntime(Copy),
+  builder: (element, annotation, buildStep) {
+    return '';
+  },
+);
