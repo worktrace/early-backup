ae717223ae26703a4f1e71a512c387db19f08a71
James Aprosail <aprosail@outlook.com>
2025-04-11 11:18:29 +0800

    feat: gen copy for smooth ui themes

---
+++ packages/smooth_ui/lib/src/themes/colors.data.g.dart
@@ -0,0 +1,35 @@
+// dart format width=80
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// Generator: DataBuilder
+// **************************************************************************
+
+part of 'colors.dart';
+
+mixin _$Copy$AreaColors implements Copyable {
+  AreaColors get _template => this as AreaColors;
+
+  @override
+  AreaColors copyWith({Color? background, Color? foreground}) => AreaColors(
+    background: background ?? _template.background,
+    foreground: foreground ?? _template.foreground,
+  );
+}
+
+mixin _$Copy$CardColors implements Copyable {
+  CardColors get _template => this as CardColors;
+
+  @override
+  CardColors copyWith({
+    Color? background,
+    Color? foreground,
+    Color? border,
+    Color? shadow,
+  }) => CardColors(
+    background: background ?? _template.background,
+    foreground: foreground ?? _template.foreground,
+    border: border ?? _template.border,
+    shadow: shadow ?? _template.shadow,
+  );
+}
