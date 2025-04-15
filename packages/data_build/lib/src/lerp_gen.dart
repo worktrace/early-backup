71a7dc04a137f16596c88eadbd6fc4e5e9d1bae4
James Aprosail <aprosail@outlook.com>
2025-04-15 09:29:49 +0800

    fix: remove conflict demo and gen names

--- packages/data_build/lib/src/lerp_gen.dart
+++ packages/data_build/lib/src/lerp_gen.dart
@@ -1,10 +1,12 @@
 import 'package:analyzer/dart/element/element.dart';
 import 'package:build/build.dart';
 import 'package:data_build/generator.dart';
-import 'package:flutter/widgets.dart' show Radius;
 import 'package:source_gen/source_gen.dart';
 
 import 'lerp.dart';
+import 'type_id.dart';
+
+const buildInLerp = <String, TypeID>{};
 
 class LerpGenerator extends AnnotationGenerator<GenerateLerp> {
   const LerpGenerator();
@@ -18,8 +20,7 @@ class LerpGenerator extends AnnotationGenerator<GenerateLerp> {
     if (element is! ConstructorElement) throw const AnnoPosException();
 
     final type = element.returnType.toString();
-    const demo = Radius.circular(12);
 
-    return '// $type _\$lerp\$$type($type a, $type b, double t) => $demo;';
+    return '// $type _\$lerp\$$type($type a, $type b, double t) => $type();';
   }
 }
