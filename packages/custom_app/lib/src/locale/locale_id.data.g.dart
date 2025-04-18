895dcf611305b2f74b5c1877d6d7838e420a004a
James Aprosail <aprosail@outlook.com>
2025-04-18 15:23:28 +0800

    fix: unnamed parameter gen for copy

---
+++ packages/custom_app/lib/src/locale/locale_id.data.g.dart
@@ -0,0 +1,23 @@
+// dart format width=80
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// Generator: PartDataBuilder
+// **************************************************************************
+
+part of 'locale_id.dart';
+
+mixin _$Copy$LocaleID implements Copyable {
+  LocaleID get _template => this as LocaleID;
+
+  @override
+  LocaleID copyWith({
+    String? languageCode,
+    String? scriptCode,
+    String? areaCode,
+  }) => LocaleID(
+    languageCode ?? _template.languageCode,
+    scriptCode: scriptCode ?? _template.scriptCode,
+    areaCode: areaCode ?? _template.areaCode,
+  );
+}
