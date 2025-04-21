d450476093618555fd9ed9b2a37c1dba4fe01e4a
James Aprosail <aprosail@outlook.com>
2025-04-21 14:01:42 +0800

    docs: optimize hover follow code structure

--- packages/smooth_ui/lib/src/effect/hover_follow.dart
+++ packages/smooth_ui/lib/src/effect/hover_follow.dart
@@ -1,5 +1,27 @@
 import 'package:flutter/widgets.dart';
 
+/// Enable hover follow effect onto this widget.
+///
+/// This widget must be a descendant of [HoverFollowArea] to enable the effect.
+/// Current widget itself will only get the position and size to hover,
+/// passing the state to the [HoverFollowArea] to ancestor to render the effect.
+/// The exact effect is not rendered inside this widget exactly.
+class HoverFollow extends StatefulWidget {
+  const HoverFollow({super.key, required this.child});
+
+  final Widget child;
+
+  @override
+  State<HoverFollow> createState() => _HoverFollowState();
+}
+
+class _HoverFollowState extends State<HoverFollow> {
+  @override
+  Widget build(BuildContext context) {
+    return widget.child;
+  }
+}
+
 /// Display the hover follow effect in this area.
 ///
 /// Wrap corresponding widgets with [HoverFollow] to enable the effect
@@ -14,22 +36,6 @@ class HoverFollowArea extends StatefulWidget {
 }
 
 class _HoverFollowAreaState extends State<HoverFollowArea> {
-  @override
-  Widget build(BuildContext context) {
-    return widget.child;
-  }
-}
-
-class HoverFollow extends StatefulWidget {
-  const HoverFollow({super.key, required this.child});
-
-  final Widget child;
-
-  @override
-  State<HoverFollow> createState() => _HoverFollowState();
-}
-
-class _HoverFollowState extends State<HoverFollow> {
   @override
   Widget build(BuildContext context) {
     return widget.child;
