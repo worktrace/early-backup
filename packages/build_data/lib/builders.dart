02d4667aa573ba36fd3e03d18db6242129182c38
James Aprosail <aprosail@outlook.com>
2025-04-22 14:03:35 +0800

    perf: avoid unnecessary lib export

--- packages/build_data/lib/builders.dart
+++ packages/build_data/lib/builders.dart
@@ -2,7 +2,7 @@ import 'package:build/build.dart';
 import 'package:nest_gen/builder.dart';
 import 'package:source_gen/source_gen.dart';
 
-import 'annotation_gen.dart';
+import '_generators.dart';
 
 Builder dataBuilder(BuilderOptions options) => LibraryBuilder(
   const PartDataBuilder([CopyGenerator(), EqualsGenerator(), LerpGenerator()]),
