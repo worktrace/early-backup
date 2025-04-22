d138238812afe9e5ccf6f66275691c7eb9ace3be
James Aprosail <aprosail@outlook.com>
2025-04-22 16:56:04 +0800

    chore: wrap extension frame

--- packages/wrap/lib/builders.dart
+++ packages/wrap/lib/builders.dart
@@ -37,24 +37,28 @@ class WrapGenerator extends AnnotationGenerator<GenerateWrap> {
   }
 
   String buildConstructor(ConstructorElement element) {
+    final type = element.returnType.toString();
+    final name = element.name;
+    final nameSuffix = name.isEmpty ? '' : '\$$name';
+
+    return 'extension $type$nameSuffix on Widget {}';
+  }
+
+  String buildWrapMethod(ConstructorElement element) {
     return '';
   }
 
-  String buildConstructorBaseMethod(ConstructorElement element) {
-    return '';
-  }
-
-  String? buildVirtualConstructor(DartObject raw) {
+  String? buildVConstructor(DartObject raw, String name) {
     final element = raw.toFunctionValue();
     return element is ConstructorElement ? buildConstructor(element) : null;
   }
 
   String? buildVConstructorBaseMethod(DartObject raw) {
     final element = raw.toFunctionValue();
-    return element is ConstructorElement ? buildConstructor(element) : null;
+    return element is ConstructorElement ? buildWrapMethod(element) : null;
   }
 
-  String? buildVConstructorSet(DartObject raw) {
+  String? buildVConstructorSet(DartObject raw, String name) {
     final method = buildVConstructorSetBaseMethods(raw);
     if (method == null) return null;
     return method;
@@ -66,10 +70,17 @@ class WrapGenerator extends AnnotationGenerator<GenerateWrap> {
       .nullIfEmpty
       ?.join('\n\n');
 
+  /// Build wrap extension output based on a top level variable.
+  ///
+  /// This top level variable is an entry to mark which constructor to build,
+  /// and such constructors are usually imported from other packages.
   String buildTopLevelVariable(TopLevelVariableElement element) {
     final raw = element.computeConstantValue();
     if (raw == null) throw WrapAnnoException(element);
-    final result = buildVirtualConstructor(raw) ?? buildVConstructorSet(raw);
+
+    final result =
+        buildVConstructor(raw, element.name) ??
+        buildVConstructorSet(raw, element.name);
     if (result != null) throw WrapAnnoException(element);
     return result!;
   }
