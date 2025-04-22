1897717bf8b3fd505e2e91d824883a0542cf1355
James Aprosail <aprosail@outlook.com>
2025-04-22 15:07:16 +0800

    chore: wrap builder entries

--- packages/wrap/lib/builders.dart
+++ packages/wrap/lib/builders.dart
@@ -1,1 +1,28 @@
+import 'package:analyzer/dart/element/element.dart';
+import 'package:build/build.dart';
+import 'package:nest_gen/builder.dart';
+import 'package:nest_gen/generator.dart';
+import 'package:source_gen/source_gen.dart';
 
+import 'annotation.dart';
+
+Builder wrapBuilder(BuilderOptions options) {
+  return LibraryBuilder(
+    const PartDataBuilder([WrapGenerator()]),
+    generatedExtension: '.wrap.g.dart',
+  );
+}
+
+class WrapGenerator extends AnnotationGenerator<GenerateWrap> {
+  const WrapGenerator();
+
+  @override
+  String build(
+    Element element,
+    ConstantReader annotation,
+    BuildStep buildStep,
+  ) {
+    if (element is! TopLevelVariableElement) throw const AnnoPosException();
+    return '// build wrap';
+  }
+}
---
+++ packages/wrap/lib/wrap.wrap.g.dart
@@ -0,0 +1,12 @@
+// dart format width=80
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// Generator: PartDataBuilder
+// **************************************************************************
+
+part of 'wrap.dart';
+
+// build wrap
+
+// build wrap
