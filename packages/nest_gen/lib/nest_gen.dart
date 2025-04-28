b1d8a525220d13e45aff840b9584955704ab65fc
James Aprosail <aprosail@outlook.com>
2025-04-28 11:32:54 +0800

    chore: optimize code file structure

--- packages/nest_gen/lib/nest_gen.dart
+++ packages/nest_gen/lib/nest_gen.dart
@@ -1,3 +1,2 @@
 export 'src/annotation_generator.dart';
 export 'src/composed_generator.dart';
-export 'src/recursive_generator.dart';
--- packages/nest_gen/lib/src/composed_generator.dart
+++ packages/nest_gen/lib/src/composed_generator.dart
@@ -1,6 +1,7 @@
 /// @docImport 'dart:async';
 library;
 
+import 'package:analyzer/dart/element/element.dart';
 import 'package:build/build.dart';
 import 'package:meta/meta.dart';
 import 'package:source_gen/source_gen.dart';
@@ -45,6 +46,33 @@ abstract class ComposedGenerator extends DirectGenerator {
     LibraryReader library,
     BuildStep buildStep,
   );
+}
+
+abstract class RecursiveGenerator extends ComposedGenerator {
+  const RecursiveGenerator();
+
+  /// Define how to generate a single element in the AST.
+  ///
+  /// All elements in the AST will be visited, which might cost a lot,
+  /// so it's supposed to return `null` when necessary as soon as possible
+  /// to tell the recursive visitor to skip redundant steps.
+  String? generateElement(Element element);
+
+  /// Visit all elements recursively and generate when necessary.
+  @override
+  Iterable<String> generateComponents(
+    LibraryReader library,
+    BuildStep buildStep,
+  ) => _generateComponents(library.element);
+
+  /// Recursive encapsulation of generating a single layer of element in AST.
+  Iterable<String> _generateComponents(Element element) sync* {
+    for (final element in element.children) {
+      final result = generateElement(element);
+      if (result != null) yield result;
+      yield* _generateComponents(element);
+    }
+  }
 }
 
 /// A generator that avoid any future operations.
