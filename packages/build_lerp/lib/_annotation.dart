afdaab6ebc54cf2a56f7a82ac5cd84f2127e14f2
James Aprosail <aprosail@outlook.com>
2025-05-06 11:48:56 +0800

    chore: build-in lerp annotation design

--- packages/build_lerp/lib/_annotation.dart
+++ packages/build_lerp/lib/_annotation.dart
@@ -1,5 +1,17 @@
+import 'package:meta/meta_meta.dart';
+
 const lerp = GenerateLerp();
 
+/// Generate lerp function according to a constructor.
+@Target({TargetKind.constructor})
 class GenerateLerp {
   const GenerateLerp();
+}
+
+const buildInLerp = GenerateBuildInLerp();
+
+/// Generate lerp function according to build-in constructors (functions).
+@Target({TargetKind.topLevelVariable})
+class GenerateBuildInLerp {
+  const GenerateBuildInLerp();
 }
