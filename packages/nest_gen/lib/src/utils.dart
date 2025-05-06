8b369178c89600f53b4c1c96a1686db49c9712b7
James Aprosail <aprosail@outlook.com>
2025-05-06 10:11:12 +0800

    docs: about annotation position exception

--- packages/nest_gen/lib/src/utils.dart
+++ packages/nest_gen/lib/src/utils.dart
@@ -1,3 +1,13 @@
+/// @docImport 'package:analyzer/dart/element/element.dart';
+/// @docImport 'package:meta/meta_meta.dart';
+library;
+
+/// Throw when the annotation position is invalid.
+///
+/// The valid position of an annotation should be indicated by the [Target]
+/// annotation provided by `package:meta`.
+/// And it is supposed to be check before using the parsed [Element]
+/// and assert the type of the source element.
 class AnnotationPositionException<T> implements Exception {
   const AnnotationPositionException();
 
