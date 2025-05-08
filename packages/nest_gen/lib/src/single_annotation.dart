bebb2e0f566cb65c9712ec337d8a78df93f0acb3
James Aprosail <aprosail@outlook.com>
2025-05-08 10:00:50 +0800

    feat: composed single annotation generator

---
+++ packages/nest_gen/lib/src/single_annotation.dart
@@ -0,0 +1,30 @@
+import 'package:analyzer/dart/element/element.dart';
+import 'package:build/build.dart';
+import 'package:source_gen/source_gen.dart';
+
+import 'composed_generator.dart';
+
+abstract class ComposedSingleAnnotationGenerator<T> extends ComposedGenerator {
+  const ComposedSingleAnnotationGenerator({this.throwOnUnresolved = false});
+
+  final bool throwOnUnresolved;
+
+  TypeChecker get typeChecker => TypeChecker.fromRuntime(T);
+
+  @override
+  Iterable<String> generateComponents(
+    LibraryReader library,
+    BuildStep buildStep,
+  ) {
+    final b = buildStep;
+    return library
+        .annotatedWith(typeChecker, throwOnUnresolved: throwOnUnresolved)
+        .map((i) => generateForAnnotatedElement(i.element, i.annotation, b));
+  }
+
+  String generateForAnnotatedElement(
+    Element element,
+    ConstantReader annotation,
+    BuildStep buildStep,
+  );
+}
