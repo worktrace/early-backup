f15ff78dce642bea359293b94d934c1e8e12e94e
James Aprosail <aprosail@outlook.com>
2025-05-08 10:58:58 +0800

    feat: gen build in lerp function name and types

---
+++ packages/build_lerp/lib/src/avoid_nullable.bil.g.dart
@@ -0,0 +1,16 @@
+// dart format width=80
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// BuildInLerpGenerator
+// **************************************************************************
+
+// ignore: implementation_imports generated.
+import 'package:build_lerp/src/build_in_anno.dart';
+
+const buildInLerpFunctions = <String, TypeID>{
+  'lerpDouble': TypeID(typeName: 'double', libraryIdentifier: 'dart:core'),
+  'lerpInt': TypeID(typeName: 'int', libraryIdentifier: 'dart:core'),
+  'lerpColor': TypeID(typeName: 'Color', libraryIdentifier: 'dart:ui'),
+  'lerpOffset': TypeID(typeName: 'Offset', libraryIdentifier: 'dart:ui'),
+};
--- packages/build_lerp/lib/src/build_in_gen.dart
+++ packages/build_lerp/lib/src/build_in_gen.dart
@@ -1,1 +1,47 @@
+import 'package:analyzer/dart/element/element.dart';
+import 'package:build/build.dart';
+import 'package:build_lerp/src/build_in_anno.dart';
+import 'package:nest_gen/nest_gen.dart';
+import 'package:source_gen/source_gen.dart';
 
+Builder buildInLerpBuilder(BuilderOptions options) => LibraryBuilder(
+  const BuildInLerpGenerator(),
+  generatedExtension: '.bil.g.dart',
+);
+
+class BuildInLerpGenerator
+    extends TopLevelSingleAnnotationGenerator<GenerateBuildInLerp> {
+  const BuildInLerpGenerator();
+
+  @override
+  String joinComponents(
+    Iterable<String> results,
+    LibraryReader library,
+    BuildStep buildStep,
+  ) {
+    return '// ignore: implementation_imports generated.\n'
+        "import '${TypeID.classLibrary}';\n\n"
+        'const buildInLerpFunctions = <String, TypeID>{${results.join(',')}};';
+  }
+
+  @override
+  String generateForAnnotatedElement(
+    Element element,
+    ConstantReader annotation,
+    BuildStep buildStep,
+  ) {
+    if (element is! FunctionElement)
+      throw const AnnotationPositionException<GenerateBuildInLerp>();
+
+    final name = element.name;
+    final type = element.returnType.toString();
+    final returnTypeID = element.returnType.element?.library?.identifier;
+
+    final paramType = "${TypeID.fieldTypeName}: '$type'";
+    final paramLibID = returnTypeID != null
+        ? "${TypeID.fieldLibraryIdentifier}: '$returnTypeID'"
+        : '';
+
+    return "'$name': TypeID($paramType, $paramLibID)";
+  }
+}
