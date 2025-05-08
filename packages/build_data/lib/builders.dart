3ba388df8bc2d35a807aef924b6f5579f6e5b63c
James Aprosail <aprosail@outlook.com>
2025-05-08 14:31:18 +0800

    feat: apply nest gen updated apis

--- packages/build_data/lib/builders.dart
+++ packages/build_data/lib/builders.dart
@@ -1,20 +1,20 @@
 import 'package:build/build.dart';
-import 'package:nest_gen/builder.dart';
+import 'package:nest_gen/nest_gen.dart';
 import 'package:source_gen/source_gen.dart';
 
 import '_generators.dart';
 
 Builder dataBuilder(BuilderOptions options) => LibraryBuilder(
-  const PartDataBuilder([CopyGenerator(), EqualsGenerator()]),
+  const PartAnnotationsBuilder([CopyGenerator(), EqualsGenerator()]),
   generatedExtension: '.data.g.dart',
 );
 
 Builder copyBuilder(BuilderOptions options) => LibraryBuilder(
-  const PartDataBuilder([CopyGenerator()]),
+  const PartAnnotationsBuilder([CopyGenerator()]),
   generatedExtension: '.copy.g.dart',
 );
 
 Builder equalsBuilder(BuilderOptions options) => LibraryBuilder(
-  const PartDataBuilder([EqualsGenerator()]),
+  const PartAnnotationsBuilder([EqualsGenerator()]),
   generatedExtension: '.equals.g.dart',
 );
