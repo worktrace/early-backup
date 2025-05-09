05fa7cc03ff0349e8deed7f95c6294f703be978a
James Aprosail <aprosail@outlook.com>
2025-05-09 16:39:49 +0800

    feat: annotate lerp package

---
+++ packages/annotate_lerp/lib/register_lerp.dart
@@ -0,0 +1,8 @@
+import 'package:meta/meta_meta.dart';
+
+const registerLerp = GenerateRegisterLerp();
+
+@Target({TargetKind.function})
+class GenerateRegisterLerp {
+  const GenerateRegisterLerp();
+}
