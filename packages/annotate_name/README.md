c4d76bd6416ef6061c8202fd70a4ed161c7820de
James Aprosail <aprosail@outlook.com>
2025-05-09 15:31:04 +0800

    feat: package annotate name

---
+++ packages/annotate_name/README.md
@@ -0,0 +1,3 @@
+# Annotate Name
+
+Annotations about how to build names with package:build_name.
---
+++ packages/annotate_name/lib/annotate_name.dart
@@ -0,0 +1,11 @@
+const name = GenerateName();
+
+class GenerateName {
+  const GenerateName();
+}
+
+const lib = GenerateLibraryIdentifier();
+
+class GenerateLibraryIdentifier {
+  const GenerateLibraryIdentifier();
+}
