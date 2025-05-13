4e4062db5c8549e352b90c0e18f643b6b17de5a1
James Aprosail <aprosail@outlook.com>
2025-05-13 10:56:44 +0800

    feat: annotation for type identifier gen

--- packages/annotate_type/lib/annotate_type.dart
+++ packages/annotate_type/lib/annotate_type.dart
@@ -1,1 +1,2 @@
+export 'src/annotation.dart';
 export 'src/type_identifier.dart';
---
+++ packages/annotate_type/lib/src/annotation.dart
@@ -0,0 +1,8 @@
+import 'package:meta/meta_meta.dart';
+
+const typeIdentifier = GenerateTypeIdentifier();
+
+@Target({TargetKind.topLevelVariable})
+class GenerateTypeIdentifier {
+  const GenerateTypeIdentifier();
+}
