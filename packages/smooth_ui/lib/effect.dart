9c4c389144825427ea83bb53bc5ed914cadf5f1a
James Aprosail <aprosail@outlook.com>
2025-05-13 11:05:54 +0800

    perf: remove squircle as provided by flutter engine

--- packages/smooth_ui/lib/effect.dart
+++ packages/smooth_ui/lib/effect.dart
@@ -2,4 +2,3 @@ export 'src/effect/hover_follow.dart';
 export 'src/effect/ripple.dart';
 export 'src/effect/ripple_area.dart';
 export 'src/effect/ripple_line.dart';
-export 'src/effect/squircle.dart';
