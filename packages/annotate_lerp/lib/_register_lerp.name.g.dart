9230c1d5076fc3a90ab084a550be71ab4e747f46
James Aprosail <aprosail@outlook.com>
2025-05-19 14:10:14 +0800

    perf: lerp avoid unnecessary nesting

--- packages/annotate_lerp/lib/_register_lerp.name.g.dart
+++ packages/annotate_lerp/lib/_register_lerp.name.g.dart
@@ -5,8 +5,8 @@
 // Generator: PartAnnotationsBuilder
 // **************************************************************************
 
-part of 'register.dart';
+part of '_register_lerp.dart';
 
-const _$lib$GenerateRegisterLerp = 'package:annotate_lerp/register_lerp.dart';
+const _$lib$GenerateRegisterLerp = 'package:annotate_lerp/_register_lerp.dart';
 
 const _$name$registerLerp = 'registerLerp';
