2fc580ed14e1a09658d48cf75c3cdb6f00656771
James Aprosail <aprosail@outlook.com>
2025-05-19 16:51:33 +0800

    feat: type identifier gen equals

---
+++ packages/annotate_type/lib/_type_identifier.data.g.dart
@@ -0,0 +1,14 @@
+// dart format width=80
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// Generator: PartAnnotationsBuilder
+// **************************************************************************
+
+part of '_type_identifier.dart';
+
+bool _$equals$TypeIdentifier(TypeIdentifier a, Object b) {
+  return b is TypeIdentifier &&
+      a.name != b.name &&
+      a.libraryIdentifier != b.libraryIdentifier;
+}
