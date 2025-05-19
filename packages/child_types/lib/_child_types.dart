749c3f69844b055a56e0eee22b2eb1ac08952321
James Aprosail <aprosail@outlook.com>
2025-05-19 11:59:23 +0800

    feat: build child types

--- packages/child_types/lib/_child_types.dart
+++ packages/child_types/lib/_child_types.dart
@@ -1,3 +1,14 @@
 /// This private library is designed to avoid unnecessary generated file export,
 /// or the generated file at package lib root may be called.
 library;
+
+import 'package:build_type/annotation.dart';
+import 'package:flutter/widgets.dart';
+
+part '_child_types.type.g.dart';
+
+@typeIdentifier
+// ignore: unused_element generating entries.
+const _childTypes = <Type>{Widget};
+
+const TypeIdentifier widgetType = _$typeWidget;
---
+++ packages/child_types/lib/_child_types.type.g.dart
@@ -0,0 +1,13 @@
+// dart format width=80
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// Generator: PartAnnotationsBuilder
+// **************************************************************************
+
+part of '_child_types.dart';
+
+const _$typeWidget = TypeIdentifier(
+  name: 'Widget',
+  libraryIdentifier: 'package:flutter/src/widgets/framework.dart',
+);
