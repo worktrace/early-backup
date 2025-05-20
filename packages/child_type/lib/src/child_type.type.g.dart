df41c74777c9befaa9bccf907ecade3ae0a5af75
James Aprosail <aprosail@outlook.com>
2025-05-20 16:48:34 +0800

    fix: support in src import in generated files

--- packages/child_type/lib/src/child_type.type.g.dart
+++ packages/child_type/lib/src/child_type.type.g.dart
@@ -5,6 +5,8 @@
 // Generator: LibraryAnnotationBuilder
 // **************************************************************************
 
+// ignore_for_file: implementation_imports generated.
+
 import 'package:annotate_type/src/type_identifier.dart';
 
 const typeWidget = TypeIdentifier(
