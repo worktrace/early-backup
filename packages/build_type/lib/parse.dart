3ddf9764671b6f6f5d65f2d77cfece16919f8920
James Aprosail <aprosail@outlook.com>
2025-05-20 10:36:16 +0800

    fix: avoid nullability suffix when parsing type

--- packages/build_type/lib/parse.dart
+++ packages/build_type/lib/parse.dart
@@ -1,11 +1,12 @@
 import 'package:analyzer/dart/element/type.dart';
 import 'package:annotate_type/_type_identifier.dart';
+import 'package:compat_utils/string.dart';
 
 export 'package:annotate_type/_type_identifier.dart';
 
 extension ParseTypeIdentifier on DartType {
   TypeIdentifier get identifier => TypeIdentifier(
-    name: toString(),
+    name: toString().removeSuffix('?'), // Avoid nullability suffix.
     libraryIdentifier: element?.library?.identifier,
   );
 }
