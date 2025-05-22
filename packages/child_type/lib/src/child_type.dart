8ace7a903458c368b78bb48f7602d65e4b290ba6
James Aprosail <aprosail@outlook.com>
2025-05-22 11:58:32 +0800

    perf: avoid unnecessary private field

--- packages/child_type/lib/src/child_type.dart
+++ packages/child_type/lib/src/child_type.dart
@@ -6,5 +6,4 @@ import 'package:annotate_type/annotate_type.dart';
 import 'package:flutter/widgets.dart';
 
 @typeIdentifier
-// ignore: unused_element generating entries.
-const _childTypes = <Type>{Widget};
+const childTypes = <Type>{Widget};
