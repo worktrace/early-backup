5bcfc09a7d12fa92726af1c51f2c8afdb7dd8af6
James Aprosail <aprosail@outlook.com>
2025-05-22 11:11:41 +0800

    docs: gen wrap anno configuration attentions

--- packages/wrap/lib/src/annotation.dart
+++ packages/wrap/lib/src/annotation.dart
@@ -9,9 +9,19 @@ const wrap = GenerateWrap();
 class GenerateWrap {
   const GenerateWrap({this.typeNameOverride, this.constructorNameOverride});
 
+  /// You may specify a special name rather than use the default type as the
+  /// generated wrap method name.
+  /// But attention that this configuration will work for all outputs
+  /// of current annotated element, that please consider it
+  /// before using such configuration when generating build-in wrap methods.
   @name
   final String? typeNameOverride;
 
+  /// You may specify a special name rather than use the
+  /// default constructor name as the generated wrap method name.
+  /// But attention that this configuration will work for all outputs
+  /// of current annotated element, that please consider it
+  /// before using such configuration when generating build-in wrap methods.
   @name
   final String? constructorNameOverride;
 
